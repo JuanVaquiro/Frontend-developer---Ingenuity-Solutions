9f458d40fc5b13bd5d24d54119d34bf89c9bbab1
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-04-27 09:59:27 -0500

    styles: container btn BannerVideo

--- src/containers/BannerVideo.jsx
+++ src/containers/BannerVideo.jsx
@@ -1,9 +1,10 @@
+import { IconComment, IconFavorite, IconOval } from '../assets/icons-group'
 import Video from '../assets/video/Final2.mp4'
 import '../styles/BannerVideo.css'
 
 const BannerVideo = () => {
   return (
-    <section>
+    <section className='main-banner'>
       <video
         className='videoDesktop'
         src={Video}
@@ -12,6 +13,26 @@ const BannerVideo = () => {
         loop
         preload='auto'
       />
+      <div className='svg-oval'>
+        <IconOval />
+      </div>
+      <div className='main-container'>
+        <div className='container-bubble'>
+          <button className='item-buble'>
+            <IconComment />
+          </button>
+        </div>
+        <div>
+          <button className='item-favorite'>
+            <div>
+              <IconFavorite />
+            </div>
+            <span>
+              JOIN US
+            </span>
+          </button>
+        </div>
+      </div>
     </section>
   )
 }
