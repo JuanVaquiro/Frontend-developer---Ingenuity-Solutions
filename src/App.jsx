be3ba29e9d0810f9fa1d4a3559ed606a01322d6a
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-04-26 19:03:35 -0500

    create  component JoinUs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import BannerSoundtrack from "./containers/BannerSoundtrack"
 import BannerVideo from "./containers/BannerVideo"
 import Events from "./containers/Events"
+import JoinUs from "./containers/JoinUs"
 import Memories from "./containers/Memories"
 import TheCrew from "./containers/TheCrew"
 
@@ -14,6 +15,7 @@ const App = () => {
       <TheCrew />
       <Memories />
       <BannerSoundtrack />
+      <JoinUs />
     </>
   )
 }
---
+++ src/containers/JoinUs.jsx
@@ -0,0 +1,57 @@
+import BtnPrimary from "../components/BtnPrimary"
+import Communication from '../assets/imgs/online-communication.png'
+import '../styles/JoinUs.css'
+
+function JoinUs() {
+  return (
+    <section className="container-join">
+      <article className="container-items-join">
+        <div className='items-text-join'>
+          <h2 className='title-section'><span>Join</span> us.</h2>
+          <p>
+            In the sea of life, there is nothing to fear if you have God as your captain. Join our crew!
+          </p>
+          <br />
+          <form action="" className="form-join">
+            <input
+              type="text"
+              id="helper-text"
+              aria-describedby="fhelper-text-explanation"
+              className="form-input"
+              placeholder="Full Name"
+            />
+            <input
+              type="email"
+              id="helper-text"
+              aria-describedby="fhelper-text-explanation"
+              className="form-input"
+              placeholder="Email Address"
+            />
+            <input
+              type="number"
+              id="helper-text"
+              aria-describedby="fhelper-text-explanation"
+              className="form-input"
+              placeholder="Cell Phone Number"
+            />
+            <textarea
+              type="text"
+              id="helper-text"
+              aria-describedby="fhelper-text-explanation"
+              className="form-input"
+              placeholder="Message"
+            />
+          </form>
+          <div className='item-btn'>
+            <BtnPrimary text='SENT' />
+          </div>
+        </div>
+        <div>
+          <img src={Communication} alt="img" />
+        </div>
+      </article>
+    </section>
+  )
+}
+
+export default JoinUs
